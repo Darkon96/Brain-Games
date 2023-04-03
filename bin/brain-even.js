cd8f8cb87f63c605e4b24228b8ba44643bd7368e
Darkon96 <steponframe2@gmail.com>
2023-04-03 23:52:11 +0300

    Your short summary about the commit

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -3,26 +3,24 @@ import readlineSync from 'readline-sync';
 import question from '../src/cli.js';
 
 const game = (names) => {
+  for (let i = 0; i < 3;) {
+    const num = Math.round(Math.random() * 100);
+    console.log(`Question: ${num}`);
+    const answer = readlineSync.question('Your answer: ');
 
-    for (let i = 0; i < 3;) {
-        let num = Math.round(Math.random() * 100);
-        console.log('Question: ' + num);
-        const answer = readlineSync.question('Your answer: ');
-
-        if (((num % 2 === 0) && (answer === 'yes')) || ((num % 2 !== 0) && (answer === 'no'))) {
-            console.log('Correct!');
-            i += 1;
-        }
-        else {
-            console.log('\'yes\' is wrong answer ;(. Correct answer was \'no\'.')
-            console.log('Let\'s try again,' + names + '!')
-            break;
-        }
-        if (i === 3) {
-            console.log('Congratulations, ' + names + '!')
-        }
+    if (((num % 2 === 0) && (answer === 'yes')) || ((num % 2 !== 0) && (answer === 'no'))) {
+      console.log('Correct!');
+      i += 1;
+    } else {
+      console.log('\'yes\' is wrong answer ;(. Correct answer was \'no\'.');
+      console.log(`Let's try again,${names}!`);
+      break;
     }
-}
+    if (i === 3) {
+      console.log(`Congratulations, ${names}!`);
+    }
+  }
+};
 game(question());
 
 export default game;
--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,10 @@
 import readlineSync from 'readline-sync';
 
 const question = () => {
-    const names = readlineSync.question('May I have your name? ');
-    console.log('Hello ' + names + '!');
-    console.log('Answer "yes" if the number is even, otherwise answer "no".');
-    return names;
-}
+  const names = readlineSync.question('May I have your name? ');
+  console.log(`Hello ${names}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  return names;
+};
 
 export default question;
