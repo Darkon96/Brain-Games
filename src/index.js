6118a736d2feea88ea82f0c408bf12d9e80bf98a
Darkon96 <steponframe2@gmail.com>
2023-04-23 23:43:18 +0300

    step 9.3

--- src/index.js
+++ src/index.js
@@ -5,23 +5,26 @@ import readlineSync from 'readline-sync';
 const roundsCount = 3;
 
 const game = (task, getQuestion) => {
-  console.log('Welcome to the Brain Games!');
+  console.log('\nWelcome to the Brain Games!');
+  console.log(task || '');
   const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
-  console.log(task);
+  console.log(`Hello, ${userName}!\n`);
+  if (!task) {
+    return;
+  }
   for (let i = 1; i <= roundsCount; i += 1) {
     const { question, answer } = getQuestion();
     console.log(`Question: ${question}`);
     const userAnswer = readlineSync.question('Your answer: ');
     if (userAnswer === answer) {
-      console.log('Correct!');
+      console.log('Correct!\n');
     } else {
       console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${answer}".`);
-      console.log(`Let's try again, ${userName}!`);
+      console.log(`\nLet's try again, ${userName}!\n`);
       return;
     }
   }
-  console.log(`Congratulations, ${userName}!`);
+  console.log(`Congratulations, ${userName}!\n`);
 };
 
 export default game;
